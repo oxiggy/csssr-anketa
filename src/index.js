a86c38f2daeab48f94dfb7841d0c637b1fde1b27
Ada V <oxiggy@awsmtek.com>
2019-06-23 22:24:30 +0300

    использование rangeslider.js вместо rangeslider-js

--- src/index.js
+++ src/index.js
@@ -3,5 +3,8 @@ require('./index.css')
 const autosize= require('autosize')
 autosize(document.querySelector('.b-textarea'))
 
-const rangeslider= require('rangeslider-js')
-rangeslider.create(document.querySelector('.b-range__input'))
+const $= require('jquery')
+const rangeslider= require('rangeslider.js')
+$('.b-range__input').rangeslider({
+    polyfill: false
+});
