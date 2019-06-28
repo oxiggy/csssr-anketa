0c0fd647cdcaef773336a7e19d6d835dc1d2ae18
Ada V <oxiggy@awsmtek.com>
2019-06-29 01:45:53 +0300

    верстка, логика слайдера

--- src/index.js
+++ src/index.js
@@ -5,6 +5,20 @@ autosize(document.querySelector('.b-textarea'))
 
 const $= require('jquery')
 const rangeslider= require('rangeslider.js')
-$('.b-range__input').rangeslider({
-    polyfill: false
-});
+
+Range(document.querySelector('.b-range'))
+
+function Range(element) {
+    const input= element.querySelector('.b-range__input');
+    $(input).rangeslider({
+        polyfill: false
+    });
+    element.querySelectorAll('.b-range__option').forEach(function (optionElement) {
+        optionElement.addEventListener('click', function () {
+            $(input)
+                .val(Number(optionElement.dataset.value))
+                .change()
+            ;
+        })
+    })
+}
